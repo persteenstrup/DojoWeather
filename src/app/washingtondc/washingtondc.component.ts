8a260738640e6c341299131ed8ddd0642060e470
Per Steenstrup <per.steenstrup@gmail.com>
2018-04-01 18:00:05 -0700

    remaining cities added

--- src/app/washingtondc/washingtondc.component.ts
+++ src/app/washingtondc/washingtondc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
+import { Weather } from '../weather';
 
 @Component({
   selector: 'app-washingtondc',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./washingtondc.component.css']
 })
 export class WashingtondcComponent implements OnInit {
+  city: Weather;
+  units: string;
+  unitHtml: string;
+  constructor(private _weatherService: WeatherService) {
+    this.city = new Weather();
+    this.unitHtml = '<span>&#8451;</span>';
+    this.units = 'celsius'
+  }
 
-  constructor() { }
+  getWeather() {
+    //makes weather call and retrieves data
+    this._weatherService.getWeather('washington,dc,usa',
+      () => { 
+        this.city = this._weatherService.city;
+        this.units = this._weatherService.units;
+        this.unitHtml = this._weatherService.unitHtml;  
+      })
+  }
+  
+  updateUnits(newUnits:string){
+    //if there is no change do nothing
+    if(this.units == newUnits){return}
+    //else update units
+    this._weatherService.updateUnits(newUnits,
+      () => {
+        this.units = this._weatherService.units;
+        this.unitHtml = this._weatherService.unitHtml;
+        this.city = this._weatherService.city;
+      })
+  }
 
   ngOnInit() {
+    this.getWeather();
   }
 
 }
